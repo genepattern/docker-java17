76f8583124992883a1e0216a0d14e7635fb5dd76
ted Liefeld <liefeld@broadinstitute.org>
2017-08-14 09:57:41 -0700

    updates for passign GP_METADATA_DIR instead of stdout and stderr filenames with paths

--- tests/selectfeaturescolumns/runBatchOverrideGpMetadataDir.sh
+++ tests/selectfeaturescolumns/runBatchOverrideGpMetadataDir.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-
+# test with override of the $GP_METADATA_DIR
 
 . runConfig.sh
-export $GP_METADATA_DIR=$WORKING_DIR/meta
+export GP_METADATA_DIR=$WORKING_DIR/meta
 mkdir $GP_METADATA_DIR
+
 . ../../common/testing_scripts/runOnBatch.sh
 
--- tests/selectfeaturescolumns/runConfig.sh
+++ tests/selectfeaturescolumns/runConfig.sh
@@ -3,15 +3,17 @@
 TEST_ROOT=$PWD
 TASKLIB=$TEST_ROOT/src
 INPUT_FILE_DIRECTORIES=$TEST_ROOT/data
-S3_ROOT=s3://moduleiotest
 WORKING_DIR=$TEST_ROOT/job_1
 
 COMMAND_LINE="java -cp $TASKLIB/SelectFeaturesColumns.jar:$TASKLIB/gp-modules.jar org.genepattern.modules.selectfeaturescolumns.SelectFeaturesColumns $INPUT_FILE_DIRECTORIES/all_aml_train.gct $WORKING_DIR/testout.gct -t1-2"
 
-S3_ROOT=s3://moduleiotest
-
+# docker local only vars
 DOCKER_CONTAINER=genepattern/java17
 
+# aws batch only vars 
+S3_ROOT=s3://moduleiotest
 JOB_DEFINITION_NAME="Java17_Oracle_Generic"
 JOB_ID=gp_job_JAVA17_$1
 JOB_QUEUE=TedTest
+
+
