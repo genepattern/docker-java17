37b929862a6efc445f98ba0ded58f7d9045fce4a
ted Liefeld <liefeld@broadinstitute.org>
2018-01-08 15:19:24 -0800

    test on beta

--- tests/selectfeaturescolumns/runConfig.sh
+++ tests/selectfeaturescolumns/runConfig.sh
@@ -11,7 +11,8 @@ COMMAND_LINE="java -cp $TASKLIB/SelectFeaturesColumns.jar:$TASKLIB/gp-modules.ja
 DOCKER_CONTAINER=genepattern/docker-java17
 
 # aws batch only vars 
-S3_ROOT=s3://moduleiotest
+#S3_ROOT=s3://moduleiotest
+S3_ROOT=s3://gpbeta
 JOB_DEFINITION_NAME="Java17_Oracle_Generic"
 JOB_ID=gp_job_JAVA17_$1
 JOB_QUEUE=TedTest
