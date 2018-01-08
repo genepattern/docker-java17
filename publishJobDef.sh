f42c1cd119f558dcf192a7da9bb682614fdbc83a
ted Liefeld <liefeld@broadinstitute.org>
2018-01-08 15:20:52 -0800

    update copyright

--- publishJobDef.sh
+++ publishJobDef.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# copyright 2017-2018 Regents of the University of California and the Broad Institute. All rights reserved.
 
 aws batch register-job-definition --cli-input-json file://jobdef.json  --profile genepattern
 
